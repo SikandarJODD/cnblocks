042e24ca08feebb10b3a4d5ead245b5dc7f3f91e
SikandarJODD <sikandar6174@gmail.com>
2025-03-18 12:22:52 +0530

    feat: Add animated and rainbow buttons, update landing page, and enhance button component

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import AnimatedButton from "$lib/components/ui/button/animated-button.svelte";
   import Button from "$lib/components/ui/button/button.svelte";
 </script>
 
@@ -39,24 +40,27 @@
   <meta name="author" content="Sikandar_Bhide" />
   <meta name="robots" content="index, follow" />
   <link rel="canonical" href="https://sv-blocks.vercel.app" />
-
 </svelte:head>
 <section>
   <div
     class="mx-4 max-w-7xl border-x px-8 py-16 [--color-border:color-mix(in_oklab,var(--color-zinc-200)_75%,transparent)] md:mx-auto dark:[--color-border:color-mix(in_oklab,var(--color-zinc-800)_60%,transparent)]"
   >
-    <div class="max-w-xl">
+    <div class="max-w-2xl">
       <h1 class="text-balance text-3xl font-bold sm:text-4xl">
-        50+ Pre-built UI Blocks
+        Svelte Shadcn Blocks
       </h1>
+      <h2 class="text-balance text-3xl mt-2 font-bold sm:text-4xl">
+        50+ Pre-built UI & Marketing Blocks
+      </h2>
       <p class="mb-6 mt-3 text-base font-montserrat text-muted-foreground">
         Speed up your workflow with responsive, pre-built UI blocks designed for
         marketing websites.
       </p>
 
-      <div class="space-x-1">
+      <div class="space-x-2 flex items-center">
         <Button href="/stats">Explore Blocks</Button>
-        <Button variant="secondary" href="/templates">Explore Templates</Button>
+        <!-- <Button variant="secondary" href="/templates">Explore Templates</Button> -->
+        <AnimatedButton href="/templates">Explore Templates</AnimatedButton>
       </div>
     </div>
   </div>
