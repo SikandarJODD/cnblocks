bbddcd665d556769290a1f60516b646d3f3c2124
Aidan Bleser <aidanbleser35@gmail.com>
2025-03-21 08:30:44 -0500

    fix: jsrepo command

--- src/routes/preview/[categoryid]/[componentid]/+page.svelte
+++ src/routes/preview/[categoryid]/[componentid]/+page.svelte
@@ -2,23 +2,26 @@
   import { page } from "$app/state";
   import { all_blocks } from "$lib/all_blocks/all_blocks";
 
-  let componentID = $derived(page.params.componentid);
-  let categoryID = $derived(page.params.categoryid);
-  let category = $derived(
+  const componentID = $derived(page.params.componentid);
+  const categoryID = $derived(page.params.categoryid);
+
+  const category = $derived(
     page.url.pathname.split("/").filter(Boolean)[0] || "/"
   );
-  let categoryBlocks = $derived.by(() => {
+
+  const categoryBlocks = $derived.by(() => {
     let categoryBlocks = all_blocks.filter(
       (block) => block.category === categoryID
     );
     return categoryBlocks;
   });
 
-  let ComponentBlock = $derived.by(() => {
+  const ComponentBlock = $derived.by(() => {
     let componentBlock = categoryBlocks.filter(
       (block) => block.title === componentID
     );
     return componentBlock[0].component;
   });
 </script>
+
 <ComponentBlock />
