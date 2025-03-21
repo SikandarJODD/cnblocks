78583697962603f27e3b451607b85b9bea6fd252
Aidan Bleser <117548273+ieedan@users.noreply.github.com>
2025-03-21 09:56:03 -0500

    Discard changes to src/lib/components/ui/button/index.ts

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -1,21 +1,19 @@
-/*
-	Installed from github/ieedan/shadcn-svelte-extras
-*/
-
 import Root, {
-	type ButtonProps,
-	type ButtonSize,
-	type ButtonVariant,
-	buttonVariants
-} from './button.svelte';
+  type ButtonProps,
+  type ButtonSize,
+  type ButtonVariant,
+  buttonVariants,
+} from "./button.svelte";
+
+
 
 export {
-	Root,
-	type ButtonProps as Props,
-	//
-	Root as Button,
-	buttonVariants,
-	type ButtonProps,
-	type ButtonSize,
-	type ButtonVariant
+  Root,
+  type ButtonProps as Props,
+  //
+  Root as Button,
+  buttonVariants,
+  type ButtonProps,
+  type ButtonSize,
+  type ButtonVariant,
 };
