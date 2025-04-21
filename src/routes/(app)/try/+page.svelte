341975601b1b5d2262cb71eed103e5cb95b2e247
SikandarJODD <sikandar6174@gmail.com>
2025-04-21 20:31:48 +0530

    feat: add FAQ component and update README with new features

--- src/routes/(app)/try/+page.svelte
+++ src/routes/(app)/try/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-  import FeatureEleven from "$lib/components/blocks/feature/feature-eleven.svelte";
-  import FeatureFourteen from "$lib/components/blocks/feature/feature-fourteen.svelte";
-  import FeatureThirteen from "$lib/components/blocks/feature/feature-thirteen.svelte";
-  import FeatureTwelve from "$lib/components/blocks/feature/feature-twelve.svelte";
+  import FaqTwo from "$lib/components/blocks/faq/faq-two.svelte";
 </script>
 
-<FeatureEleven />
-<FeatureTwelve />
-<FeatureThirteen />
-<FeatureFourteen />
+<FaqTwo />
