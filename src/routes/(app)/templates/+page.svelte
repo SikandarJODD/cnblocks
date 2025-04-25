37a640cedd16a1e302cb3c1dad3af94f35da972d
SikandarJODD <sikandar6174@gmail.com>
2025-04-26 00:41:51 +0530

    fix: clean up markup and replace RainbowButton with Button for consistency

--- src/routes/(app)/templates/+page.svelte
+++ src/routes/(app)/templates/+page.svelte
@@ -51,9 +51,7 @@
       "-z-40"
     )}
   />
-  <h1 class="text-3xl font-bold mt-4 capitalize mb-2">
-    Startup Template
-  </h1>
+  <h1 class="text-3xl font-bold mt-4 capitalize mb-2">Startup Template</h1>
   <p class="text-muted-foreground text-md md:text-lg">
     The Ultimate Landing Page for your Startup.
   </p>
@@ -61,13 +59,14 @@
     <img
       src="/landing-page.png"
       alt="landing page"
-      class="h-fit object-cover border object-top rounded-2xl border border-zinc-700/20 shadow-md dark:shadow-zinc-950"
+      class="h-fit object-cover border object-top rounded-2xl border-zinc-700/20 shadow-md dark:shadow-zinc-950"
     />
   </div>
   <div class="my-8 w-full flex items-center gap-3">
-    <RainbowButton
+    <Button
+      target="_blank"
       href="https://buy.polar.sh/polar_cl_AjEE4uRd34lOYFgOsLlGXCtyHfPqrwL6rXx5G3qPNAi"
-      class="w-fit h-11">Buy Now</RainbowButton
+      >Buy Now</Button
     >
     <Button
       href="https://sv-landing-pg.vercel.app"
@@ -76,30 +75,6 @@
       size="lg"
     >
       Live Demo
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 24 24"
-        class="sm:opacity-80"
-        fill="none"
-        color="currentColor"
-      >
-        <path
-          d="M11.0991 3.00012C7.45013 3.00669 5.53932 3.09629 4.31817 4.31764C3.00034 5.63568 3.00034 7.75704 3.00034 11.9997C3.00034 16.2424 3.00034 18.3638 4.31817 19.6818C5.63599 20.9999 7.75701 20.9999 11.9991 20.9999C16.241 20.9999 18.3621 20.9999 19.6799 19.6818C20.901 18.4605 20.9906 16.5493 20.9972 12.8998"
-          stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-        <path
-          d="M20.556 3.49612L11.0487 13.0586M20.556 3.49612C20.062 3.00151 16.7343 3.04761 16.0308 3.05762M20.556 3.49612C21.05 3.99074 21.0039 7.32273 20.9939 8.02714"
-          stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>
-      </svg>
     </Button>
   </div>
   <Separator />
