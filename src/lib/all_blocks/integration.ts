e5e7c6f142db0dfcffd367d542c1d1b76f94eeb3
SikandarJODD <sikandar6174@gmail.com>
2025-05-20 02:23:30 +0530

    feat: Removed Logo and added SVG added CLI Support for all mist components

--- src/lib/all_blocks/integration.ts
+++ src/lib/all_blocks/integration.ts
@@ -2,24 +2,24 @@ import type { Block } from "../../types/blocks";
 import { getBlock } from "$lib";
 
 // comps
-import IntegrationOne from "$lib/components/blocks/integrations/integration-one.svelte";
-import IntegrationTwo from "$lib/components/blocks/integrations/integration-two.svelte";
-import IntegrationThree from "$lib/components/blocks/integrations/integration-three.svelte";
-import IntegrationFour from "$lib/components/blocks/integrations/integration-four.svelte";
-import IntegrationFive from "$lib/components/blocks/integrations/integration-five.svelte";
-import IntegrationSix from "$lib/components/blocks/integrations/integration-six.svelte";
-import IntegrationSeven from "$lib/components/blocks/integrations/integration-seven.svelte";
-import IntegrationEight from "$lib/components/blocks/integrations/integration-eight.svelte";
+import IntegrationOne from "$lib/components/blocks/integration/integration-one.svelte";
+import IntegrationTwo from "$lib/components/blocks/integration/integration-two.svelte";
+import IntegrationThree from "$lib/components/blocks/integration/integration-three.svelte";
+import IntegrationFour from "$lib/components/blocks/integration/integration-four.svelte";
+import IntegrationFive from "$lib/components/blocks/integration/integration-five.svelte";
+import IntegrationSix from "$lib/components/blocks/integration/integration-six.svelte";
+import IntegrationSeven from "$lib/components/blocks/integration/integration-seven.svelte";
+import IntegrationEight from "$lib/components/blocks/integration/integration-eight.svelte";
 
 // codes
-import IntegrationOneCode from "$lib/components/blocks/integrations/integration-one.svelte?raw";
-import IntegrationTwoCode from "$lib/components/blocks/integrations/integration-two.svelte?raw";
-import IntegrationThreeCode from "$lib/components/blocks/integrations/integration-three.svelte?raw";
-import IntegrationFourCode from "$lib/components/blocks/integrations/integration-four.svelte?raw";
-import IntegrationFiveCode from "$lib/components/blocks/integrations/integration-five.svelte?raw";
-import IntegrationSixCode from "$lib/components/blocks/integrations/integration-six.svelte?raw";
-import IntegrationSevenCode from "$lib/components/blocks/integrations/integration-seven.svelte?raw";
-import IntegrationEightCode from "$lib/components/blocks/integrations/integration-eight.svelte?raw";
+import IntegrationOneCode from "$lib/components/blocks/integration/integration-one.svelte?raw";
+import IntegrationTwoCode from "$lib/components/blocks/integration/integration-two.svelte?raw";
+import IntegrationThreeCode from "$lib/components/blocks/integration/integration-three.svelte?raw";
+import IntegrationFourCode from "$lib/components/blocks/integration/integration-four.svelte?raw";
+import IntegrationFiveCode from "$lib/components/blocks/integration/integration-five.svelte?raw";
+import IntegrationSixCode from "$lib/components/blocks/integration/integration-six.svelte?raw";
+import IntegrationSevenCode from "$lib/components/blocks/integration/integration-seven.svelte?raw";
+import IntegrationEightCode from "$lib/components/blocks/integration/integration-eight.svelte?raw";
 
 let codes = [
   IntegrationOneCode,
