de434e8597efa344ad2cc0c922f765eb421d5654
SikandarJODD <sikandar6174@gmail.com>
2025-05-25 19:02:23 +0530

    fix: Correct CSS syntax for pre.shiki styles in code component

--- src/lib/components/web/code/code.svelte
+++ src/lib/components/web/code/code.svelte
@@ -172,7 +172,7 @@
   }
 
   :global(pre.shiki) {
-    @apply overflow-auto rounded-none bg-inherit py-4 text-xs
+    @apply overflow-auto rounded-none bg-inherit py-4 text-xs;
     max-height: min(100%, 650px);
     -ms-overflow-style: none; /* IE and Edge */
     scrollbar-width: none; /* Firefox */
